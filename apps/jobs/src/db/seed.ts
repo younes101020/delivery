c709762b47a31d4bc41712a11203daf67504bb81
younes101020 <senyoupl@icloud.com>
2024-11-24 17:43:56 +0100

    chore: fix ci

--- apps/jobs/src/db/seed.ts
+++ apps/jobs/src/db/seed.ts
@@ -8,6 +8,10 @@ import { users } from "./schema";
 async function main() {
   const db = drizzle(env.DATABASE_URL);
   await seed(db, { users });
+  process.exit(0);
 }
 
-main();
+main().catch((err) => {
+  console.error(`Error while seeding database: ${err}`);
+  process.exit(1);
+});
