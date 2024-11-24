02202f1af7e3b9c0d3e15098d4b8c3eb78f881bf
younes101020 <senyoupl@icloud.com>
2024-11-24 18:07:28 +0100

    chore: fix ci

--- apps/jobs/src/lib/constants.ts
+++ apps/jobs/src/lib/constants.ts
@@ -3,7 +3,7 @@ import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 export const ZOD_ERROR_MESSAGES = {
   REQUIRED: "Required",
-  EXPECTED_NUMBER: "Expected number, received nan",
+  EXPECTED_NUMBER: "Expected number received nan",
   NO_UPDATES: "No updates provided",
 };
 
