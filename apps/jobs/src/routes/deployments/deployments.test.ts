4dd3a428d4e5ff10b024368b2570be31cdc5463c
younes101020 <senyoupl@icloud.com>
2024-12-17 20:38:59 +0100

    chore: fix ci

--- apps/jobs/src/routes/deployments/deployments.test.ts
+++ apps/jobs/src/routes/deployments/deployments.test.ts
@@ -31,7 +31,7 @@ describe("applications routes", () => {
     expect(response.status).toBe(422);
     if (response.status === 422) {
       const json = await response.json();
-      expect(json.error.issues[0].path[0]).toBe("fqdn");
+      expect(json.error.issues[0].path[0]).toBe("repoUrl");
       expect(json.error.issues[0].message).toBe(ZOD_ERROR_MESSAGES.REQUIRED);
     }
   });
