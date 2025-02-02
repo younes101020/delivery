88a62697b1e92abb4566e916feabd7effdebb015
younes101020 <senyoupl@icloud.com>
2025-02-02 17:25:34 +0100

    chore: handle validation and update descriptions on screen endpoint

--- apps/jobs/src/routes/screenshots/screenshots.routes.ts
+++ apps/jobs/src/routes/screenshots/screenshots.routes.ts
@@ -1,6 +1,7 @@
 import { createRoute } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent, jsonContentRequired } from "stoker/openapi/helpers";
+import { createErrorSchema } from "stoker/openapi/schemas";
 
 import { selectScreenshotSchema, selectUrlScreenshotSchema } from "@/db/dto/screenshots.dto";
 import { notFoundSchema } from "@/lib/constants";
@@ -9,18 +10,19 @@ const tags = ["Website screenshot"];
 
 export const getImageUrl = createRoute({
   path: "/screenshots",
-  description: "Use this endpoint to upload the image of a website; it will return a public link to the image.",
+  description: "Use this endpoint to upload the screenshot image of a website; it will return a public link to the image.",
   method: "post",
   request: {
     body: jsonContentRequired(
       selectScreenshotSchema,
-      "the website URL to screenshot and the id of the application",
+      "the website URL to take the screenshot from and the id of the application",
     ),
   },
   tags,
   responses: {
     [HttpStatusCodes.OK]: jsonContent(selectUrlScreenshotSchema, "The uploaded image url"),
     [HttpStatusCodes.NOT_FOUND]: jsonContent(notFoundSchema, "Application not found"),
+    [HttpStatusCodes.UNPROCESSABLE_ENTITY]: jsonContent(createErrorSchema(selectScreenshotSchema), "The validation error(s)"),
   },
 });
 
