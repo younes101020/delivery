0c4a6722e475f3ea512c28a5bd0ced1a53946628
younes101020 <senyoupl@icloud.com>
2025-02-09 17:12:56 +0100

    feat: delegate job timeout to process functions

--- apps/jobs/src/routes/deployments/lib/tasks/worker.ts
+++ apps/jobs/src/routes/deployments/lib/tasks/worker.ts
@@ -2,27 +2,10 @@ import { type Job, UnrecoverableError } from "bullmq";
 
 import { jobs } from "./deploy/jobs";
 
-const MAX_TTL = 1200000; // 20min
-const TTL_EXIT_CODE = 10;
-
 export default async function Worker(job: Job) {
-  let hasCompleted = false;
-  const harKillTimeout = setTimeout(() => {
-    if (!hasCompleted) {
-      process.exit(TTL_EXIT_CODE);
-    }
-  }, MAX_TTL);
-
   const processor = jobs[job.name];
-
-  try {
-    if (!processor) {
-      throw new UnrecoverableError(`No processor found for job: ${job.name}`);
-    }
-    await processor(job);
-    hasCompleted = true;
+  if (!processor) {
+    throw new UnrecoverableError(`No processor found for job: ${job.name}`);
   }
-  finally {
-    clearTimeout(harKillTimeout);
-  }
+  return processor(job);
 }
