461a216af6032c6a61a90a6448d7673afdc505fc
younes101020 <senyoupl@icloud.com>
2025-02-28 21:49:50 +0100

    ci: fix ci

--- apps/jobs/src/routes/databases/lib/remote-docker/utils.ts
+++ apps/jobs/src/routes/databases/lib/remote-docker/utils.ts
@@ -1,14 +1,22 @@
 import Docker from "dockerode";
 
+import type { DatabaseSchema } from "@/db/dto/databases.dto";
+
 import { loadConfig } from "@/lib/ssh/utils";
 
 const sshConfig = await loadConfig();
 export const docker = new Docker({ protocol: "ssh", username: sshConfig.username, sshOptions: sshConfig });
 
 export async function getDatabasesContainers() {
-  // const options = { filters: { label: ["delivery.resource=database"] } };
   const dbContainers = await docker.listContainers({ all: true });
-  return dbContainers.map(({ Image, Id, State, Status, Created }) => ({ name: Image.split(":")[0], id: Id, status: Status, state: State, createdAt: Created, isProcessing: false }));
+  return dbContainers.map(({ Image, Id, State, Status, Created }) => ({
+    name: Image.split(":")[0],
+    id: Id,
+    status: Status,
+    state: State as DatabaseSchema["state"],
+    createdAt: Created,
+    isProcessing: false,
+  }));
 }
 
 export async function stopDatabaseContainer(containerId: string) {
