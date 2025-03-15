aca54aa752d6fc06471f0ffb3c3f778525a5e9e5
younes101020 <senyoupl@icloud.com>
2025-03-15 18:22:49 +0100

    chore: update app test

--- apps/jobs/src/routes/applications/__tests__/applications.integration.test.ts
+++ apps/jobs/src/routes/applications/__tests__/applications.integration.test.ts
@@ -10,35 +10,6 @@ import { it } from "./fixtures";
 const client = testClient(createApp().route("/", router));
 
 describe("application routes / integration test", () => {
-  it("get /applications/{slug} return application info when payload is submited", async ({ applicationName }) => {
-    const response = await client.applications[":slug"].$get(
-      {
-        param: {
-          slug: applicationName,
-        },
-      },
-    );
-    if (response.status === 200) {
-      const json = await response.json();
-      expect(json.name).toBe(applicationName);
-    }
-  });
-
-  it("get /applications/{slug} validates the param application id", async () => {
-    const response = await client.applications[":slug"].$get(
-      {
-        // @ts-expect-error test purpose
-        param: { slug: 10 },
-      },
-    );
-    expect(response.status).toBe(422);
-    if (response.status === 422) {
-      const json = await response.json();
-      expect(json.error.issues[0].path[0]).toBe("slug");
-      expect(json.error.issues[0].message).toBe(ZOD_ERROR_MESSAGES.EXPECTED_NUMBER);
-    }
-  });
-
   it("patch /applications/{slug} return application info when payload is submited", async ({ applicationName }) => {
     const newPort = 7458;
     const response = await client.applications[":slug"].$patch(
