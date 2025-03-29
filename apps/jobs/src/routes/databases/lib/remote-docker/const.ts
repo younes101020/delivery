d1df602cc59ebc1d964ac6bca8acb7cd6c6622f3
younes101020 <senyoupl@icloud.com>
2025-03-29 23:58:42 +0100

    chore: define constants

--- apps/jobs/src/routes/databases/lib/remote-docker/const.ts
+++ apps/jobs/src/routes/databases/lib/remote-docker/const.ts
@@ -1,9 +1,19 @@
+export const DATABASE_INSTANCE_REPLICAS = 1;
+
 export const credentialsEnvKeys = {
   postgres: ["POSTGRES_USER", "POSTGRES_PASSWORD"],
   mongo: ["MONGO_INITDB_ROOT_USERNAME", "MONGO_INITDB_ROOT_PASSWORD"],
   redis: ["REDIS_PASSWORD"],
   mariadb: ["MARIADB_ROOT_PASSWORD"],
   mysql: ["MYSQL_ROOT_PASSWORD", "MYSQL_USER", "MYSQL_PASSWORD"],
-};
+} as const;
 
 export const databases = Object.keys(credentialsEnvKeys);
+
+export const UNSUPPORTED_DATABASES_ERROR_MESSAGE = "Database type not supported.";
+export const DATABASES_CONTAINER_NOT_FOUND_ERROR_MESSAGE = "Database container not found.";
+export const DEFAULT_DATABASES_CREDENTIALS_ENV_VAR_NOT_FOUND_ERROR_MESSAGE = "This database does not have default credentials, you need to set them manually.";
+
+export const NO_APPLICATION_TO_LINK_WITH_ERROR_MESSAGE = "Application target service not found.";
+
+export const NO_CONTAINER_SERVICE_ERROR_MESSAGE = "No container found, make sure the service is a container.";
--- apps/jobs/src/routes/databases/lib/remote-docker/utils.ts
+++ apps/jobs/src/routes/databases/lib/remote-docker/utils.ts
@@ -6,9 +6,10 @@ import * as HttpStatusCodes from "stoker/http-status-codes";
 import type { Database } from "@/db/dto";
 
 import { getDocker } from "@/lib/remote-docker";
+import { generateRandomString } from "@/lib/utils";
 import { getApplicationService } from "@/routes/applications/lib/remote-docker/utils";
 
-import { credentialsEnvKeys, databases } from "./const";
+import { credentialsEnvKeys, databases, DATABASES_CONTAINER_NOT_FOUND_ERROR_MESSAGE, DEFAULT_DATABASES_CREDENTIALS_ENV_VAR_NOT_FOUND_ERROR_MESSAGE, NO_APPLICATION_TO_LINK_WITH_ERROR_MESSAGE, NO_CONTAINER_SERVICE_ERROR_MESSAGE, UNSUPPORTED_DATABASES_ERROR_MESSAGE } from "./const";
 import { withRestDatabaseService } from "./middleware";
 
 export const getDatabaseCredentialsEnvVarsByName = withRestDatabaseService(
@@ -16,15 +17,18 @@ export const getDatabaseCredentialsEnvVarsByName = withRestDatabaseService(
     const dbName = dbService.Spec?.Name;
     const databaseExist = assertNameIsDatabaseNameGuard(dbName);
     if (!databaseExist)
-      throw new Error("This database name is not supported yet.");
+      throw new Error(UNSUPPORTED_DATABASES_ERROR_MESSAGE);
+
     const envVarKeys = credentialsEnvKeys.postgres;
     const taskTemplate = dbService.Spec?.TaskTemplate;
     const containerSpecExist = assertTaskTemplateIsContainerTaskSpecGuard(taskTemplate);
     if (!containerSpecExist)
-      throw new Error("Task template is not a container task spec");
+      throw new Error(NO_CONTAINER_SERVICE_ERROR_MESSAGE);
+
     const envVars = taskTemplate.ContainerSpec?.Env;
     if (!envVars || envVars.length === 0)
-      throw new Error("No environment variables found in the database service");
+      throw new Error(DEFAULT_DATABASES_CREDENTIALS_ENV_VAR_NOT_FOUND_ERROR_MESSAGE);
+
     return envVars.filter(envVar => envVarKeys.some(key => envVar.includes(key)));
   },
 );
@@ -34,7 +38,7 @@ export async function getDatabaseEnvVarsByEnvVarKeys(containerId: string, envVar
   const containerMetadata = await docker.getContainer(containerId).inspect();
 
   if (!containerMetadata)
-    throw new HTTPException(HttpStatusCodes.NOT_FOUND, { message: "Database container not found" });
+    throw new HTTPException(HttpStatusCodes.NOT_FOUND, { message: DATABASES_CONTAINER_NOT_FOUND_ERROR_MESSAGE });
 
   const envVars = containerMetadata.Config.Env;
 
@@ -44,7 +48,7 @@ export async function getDatabaseEnvVarsByEnvVarKeys(containerId: string, envVar
 export async function addEnvironmentVariableToAppService(applicationName: string, plainEnv: string) {
   const appService = await getApplicationService({ name: [applicationName] });
   if (!appService)
-    throw new HTTPException(HttpStatusCodes.INTERNAL_SERVER_ERROR, { message: "Application target service not found" });
+    throw new HTTPException(HttpStatusCodes.INTERNAL_SERVER_ERROR, { message: NO_APPLICATION_TO_LINK_WITH_ERROR_MESSAGE });
 
   const container = appService.Spec?.TaskTemplate as Dockerode.ContainerTaskSpec;
   const currentEnvs = typeof container.ContainerSpec?.Env === "object" ? container.ContainerSpec.Env : [];
