5436860d7ddabfd7a414a88be9c355d25c1bf620
younes101020 <senyoupl@icloud.com>
2025-04-05 21:20:58 +0200

    ci: fix test

--- apps/jobs/src/routes/applications/__tests__/applications.integration.test.ts
+++ apps/jobs/src/routes/applications/__tests__/applications.integration.test.ts
@@ -11,10 +11,10 @@ const client = testClient(createApp().route("/", router));
 describe("application routes / integration test", () => {
   it("patch /applications/{slug} return application info when payload is submited", async ({ applicationName }) => {
     const newPort = 7458;
-    const response = await client.applications[":slug"].$patch(
+    const response = await client.applications[":name"].$patch(
       {
         param: {
-          slug: applicationName,
+          name: applicationName,
         },
         json: {
           applicationData: { port: newPort },
