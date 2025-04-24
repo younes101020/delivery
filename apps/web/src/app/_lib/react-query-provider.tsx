8dd739d1287a4f7b05b2b57efadb7f828f7800b5
younes101020 <senyoupl@icloud.com>
2025-04-24 20:20:17 +0200

    chore: add tanstack query dev tools

--- apps/web/src/app/_lib/react-query-provider.tsx
+++ apps/web/src/app/_lib/react-query-provider.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -42,6 +43,9 @@ export default function ReactQueryProviders({ children }: { children: React.Reac
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
