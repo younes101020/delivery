f1eb54bfb91b406c446ef3e64e89430c20598b51
younes101020 <senyoupl@icloud.com>
2025-07-09 00:11:22 +0200

    chore: remove useless seed function

--- apps/jobs/src/db/migrate.ts
+++ apps/jobs/src/db/migrate.ts
@@ -2,10 +2,8 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import path from "node:path";
 
 import env from "@/env";
-import { databases as databasesConst } from "@/routes/databases/lib/remote-docker/const";
 
 import { db } from ".";
-import { databases } from "./schema";
 
 async function main() {
   const migrationsFolder = path.join(process.cwd(), env.NODE_ENV === "production" ? "/apps/jobs/src/db/migrations" : "/src/db/migrations");
@@ -14,7 +12,6 @@ async function main() {
     migrationsFolder,
   });
 
-  await seedWithDatabaseConstants();
   // eslint-disable-next-line no-console
   console.log("🛢 Database resource migrations applied successfully");
 
@@ -25,18 +22,3 @@ main().catch((err) => {
   console.error(`Error when applying migrations: ${err}`);
   process.exit(1);
 });
-
-async function seedWithDatabaseConstants() {
-  const alreadyExists = await db.select({ id: databases.id }).from(databases);
-  if (alreadyExists.length === 0) {
-    return Promise.all(
-      databasesConst.map(async ({ image, port, databaseCredentialsEnvVar }) => {
-        return db.insert(databases).values({
-          image,
-          port,
-          credentialsEnvVar: databaseCredentialsEnvVar,
-        });
-      }),
-    );
-  }
-}
