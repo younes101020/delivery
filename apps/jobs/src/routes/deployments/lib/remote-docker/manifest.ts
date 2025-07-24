3e628888cc45ff9d3080c0c4b4347caacb48c2d1
younes101020 <senyoupl@icloud.com>
2025-07-24 22:02:10 +0200

    chore: remove deprecated trarfik labels

--- apps/jobs/src/routes/deployments/lib/remote-docker/manifest.ts
+++ apps/jobs/src/routes/deployments/lib/remote-docker/manifest.ts
@@ -41,7 +41,7 @@ export function createApplicationServiceSpec({ applicationName, image, port, pla
     Labels: {
       "resource": "application",
       "traefik.enable": "true",
-      "traefik.docker.network": CLUSTER_NETWORK_NAME,
+      "traefik.swarm.network": CLUSTER_NETWORK_NAME,
       [`traefik.http.routers.${applicationName}.entrypoints`]: "web",
       [`traefik.http.routers.${applicationName}.rule`]: `Host(\`${fqdn}\`)`,
       [`traefik.http.services.${applicationName}.loadbalancer.server.port`]: port.toString(),
