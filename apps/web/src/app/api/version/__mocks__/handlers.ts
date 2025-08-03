9bbf6e913cacac28360ce4ff28a35bd8d8d8659b
younes101020 <senyoupl@icloud.com>
2025-08-04 00:39:14 +0200

    chore: merge mock handlers

--- apps/web/src/app/api/version/__mocks__/handlers.ts
+++ apps/web/src/app/api/version/__mocks__/handlers.ts
@@ -1,8 +1,11 @@
 import { http, HttpResponse } from "msw";
 
+import { env } from "@/env";
+
+const DELIVERY_VERSION_URL = new URL("/version", env.JOBS_API_BASEURL).toString();
+
 export default [
-  http.get(/\/version/, () => {
-    console.log("Mocked GET /version request");
+  http.get(DELIVERY_VERSION_URL, () => {
     return HttpResponse.json({
       version: "1.0.0",
       imageDigest: "sha256:1234567890abcdef",
