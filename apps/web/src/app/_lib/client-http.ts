535579b95c489cc1e58decc75cac43850261f917
younes101020 <senyoupl@icloud.com>
2025-08-08 16:06:00 +0200

    chore(web): forward jwt to api

--- apps/web/src/app/_lib/client-http.ts
+++ apps/web/src/app/_lib/client-http.ts
@@ -7,11 +7,19 @@ import { env } from "@/env";
 export async function client() {
   const cookieStore = await cookies();
   const onboardingCookie = cookieStore.get("skiponboarding");
+  const sessionCookie = cookieStore.get("session");
+
+  const cookieString = [
+    onboardingCookie && `${onboardingCookie.name}=${onboardingCookie.value}`,
+    sessionCookie && `${sessionCookie.name}=${sessionCookie.value}`,
+  ]
+    .filter(Boolean)
+    .join("; ");
 
   return hcWithType(`${env.JOBS_API_BASEURL}`, {
     headers: {
       Authorization: `Bearer ${env.JOBS_BEARER_TOKEN}`,
-      Cookie: onboardingCookie ? `${onboardingCookie.name}=${onboardingCookie.value}` : "",
+      Cookie: cookieString,
     },
     init: {
       credentials: "include",
--- apps/web/src/app/_lib/session.ts
+++ apps/web/src/app/_lib/session.ts
@@ -6,14 +6,10 @@ import { signToken, verifyToken } from "@delivery/auth";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-// eslint-disable-next-line node/no-process-env
-const key = new TextEncoder().encode(process.env.AUTH_SECRET);
-
-interface SessionData {
-  user: { id: number };
-  expires: string;
-}
-
+/*
+  This function checks the session cookie in the request.
+  If the session is valid, it refreshes the session cookie with a new expiration date.
+*/
 export async function checkSession(request: NextRequest) {
   const sessionCookie = request.cookies.get("session");
 
@@ -54,7 +50,7 @@ export async function getSession() {
 export async function setSession(user: SelectUserSchema) {
   const expiresInOneDay = new Date(Date.now() + 24 * 60 * 60 * 1000);
   const session: SessionData = {
-    user: { id: user.id },
+    user: { id: user.id, role: user.role },
     expires: expiresInOneDay.toISOString(),
   };
   const encryptedSession = await signToken(session);
