f65c3f326ed4c62421fa540140986bcdc6725883
younes101020 <senyoupl@icloud.com>
2025-08-22 17:00:02 +0200

    fix(web): display deployment status on sidebar

--- apps/web/src/app/(dashboard)/dashboard/layout.tsx
+++ apps/web/src/app/(dashboard)/dashboard/layout.tsx
@@ -2,11 +2,13 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { enableHTTPMocks } from "@/../__mocks__/utils";
 import { AppSidebar } from "@/app/_components/app-sidebar";
+import { SubscribeToSSE } from "@/app/_components/subscribe-to-sse";
 import { Breadcrumb, BreadcrumbList } from "@/app/_components/ui/breadcrumb";
 import { Separator } from "@/app/_components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/app/_components/ui/sidebar";
 import { getQueryClient } from "@/app/_lib/get-rsc-query-client";
 import { getDeliveryVersionInfo } from "@/app/_lib/jobs/queries";
+import { env } from "@/env";
 
 import { DynamicBreadcrumb } from "./_components/breadcrumb";
 
@@ -17,6 +19,7 @@ export default function Layout({
 }>) {
   enableHTTPMocks();
   const queryClient = getQueryClient();
+  const baseUrl = env.WEB_BASE_URL;
 
   queryClient.prefetchQuery({
     queryKey: ["version"],
@@ -24,24 +27,26 @@ export default function Layout({
   });
 
   return (
-    <SidebarProvider>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <AppSidebar />
-      </HydrationBoundary>
-      <SidebarInset>
-        <div className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <DynamicBreadcrumb />
-              </BreadcrumbList>
-            </Breadcrumb>
+    <SubscribeToSSE baseUrl={baseUrl}>
+      <SidebarProvider>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <AppSidebar />
+        </HydrationBoundary>
+        <SidebarInset>
+          <div className="flex h-16 shrink-0 items-center gap-2">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <DynamicBreadcrumb />
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
           </div>
-        </div>
-        <div className="h-full px-4">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
+          <div className="h-full px-4">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </SubscribeToSSE>
   );
 }
