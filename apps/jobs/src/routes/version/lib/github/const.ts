d9f87c79efa64295968f0bfca2d8550c7f0bf595
younes101020 <senyoupl@icloud.com>
2025-08-25 23:38:04 +0200

    fix(version): increase github rate limit by using access token

--- apps/jobs/src/routes/version/lib/github/const.ts
+++ apps/jobs/src/routes/version/lib/github/const.ts
@@ -1,2 +1,8 @@
 export const DELIVERY_LATEST_URL = "https://api.github.com/repos/younes101020/delivery/releases/latest";
 export const DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL = "https://api.github.com/repos/younes101020/delivery/actions/runs?status=in_progress";
+
+/*
+    Note: This GitHub token used in the fetch calls is a public token, meaning
+    it does not expire and is not sensitive because it has no restricted access
+*/
+export const GH_ACCESS_TOKEN = "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";
--- apps/jobs/src/routes/version/lib/github/index.ts
+++ apps/jobs/src/routes/version/lib/github/index.ts
@@ -1,14 +1,22 @@
-import { DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, DELIVERY_LATEST_URL } from "./const";
+import { DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, DELIVERY_LATEST_URL, GH_ACCESS_TOKEN } from "./const";
 import { getVersionFromTag } from "./utils";
 
 export async function getLatestDeliveryVersion() {
-  const app = await fetch(DELIVERY_LATEST_URL).then(res => res.json());
+  const app = await fetch(DELIVERY_LATEST_URL, {
+    headers: {
+      Authorization: `token ${GH_ACCESS_TOKEN}`,
+    },
+  }).then(res => res.json());
   if (app.tag_name)
     return getVersionFromTag(app.tag_name);
   return "1.0.0";
 }
 
 export async function isPipelineInProgress() {
-  const response = await fetch(DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL).then(res => res.json());
+  const response = await fetch(DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, {
+    headers: {
+      Authorization: `token ${GH_ACCESS_TOKEN}`,
+    },
+  }).then(res => res.json());
   return Number.parseInt(response.total_count) > 0;
 }
