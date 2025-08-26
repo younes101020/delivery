72b27046ed4c2aa808a24fd971dd15d4f99b1ca1
younes101020 <senyoupl@icloud.com>
2025-08-26 14:14:54 +0200

    fix(version): reach github as unauth if rate limit is reached fallback to v0 so current instance is infered as up to date

--- apps/jobs/src/routes/version/lib/github/const.ts
+++ apps/jobs/src/routes/version/lib/github/const.ts
@@ -1,8 +1,2 @@
 export const DELIVERY_LATEST_URL = "https://api.github.com/repos/younes101020/delivery/releases/latest";
 export const DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL = "https://api.github.com/repos/younes101020/delivery/actions/runs?status=in_progress";
-
-/*
-    This fine-grained GitHub access token does not expire
-    and is not sensitive because it has no restricted access (read-only access to public repositories).
-*/
-export const GH_ACCESS_TOKEN = "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";
--- apps/jobs/src/routes/version/lib/github/index.ts
+++ apps/jobs/src/routes/version/lib/github/index.ts
@@ -1,24 +1,14 @@
-import { DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, DELIVERY_LATEST_URL, GH_ACCESS_TOKEN } from "./const";
+import { DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, DELIVERY_LATEST_URL } from "./const";
 import { getVersionFromTag } from "./utils";
 
 export async function getLatestDeliveryVersion() {
-  const app = await fetch(DELIVERY_LATEST_URL, {
-    headers: {
-      Authorization: `Bearer ${GH_ACCESS_TOKEN}`,
-      Accept: "application/vnd.github.v3+json",
-    },
-  }).then(res => res.json());
+  const app = await fetch(DELIVERY_LATEST_URL).then(res => res.json());
   if (app.tag_name)
     return getVersionFromTag(app.tag_name);
-  return "1.0.0";
+  return "0.0.0";
 }
 
 export async function isPipelineInProgress() {
-  const response = await fetch(DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL, {
-    headers: {
-      Authorization: `Bearer ${GH_ACCESS_TOKEN}`,
-      Accept: "application/vnd.github.v3+json",
-    },
-  }).then(res => res.json());
+  const response = await fetch(DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL).then(res => res.json());
   return Number.parseInt(response.total_count) > 0;
 }
