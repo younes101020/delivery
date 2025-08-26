21c2b676419be45302a500cfba4e3d51e6995b8f
younes101020 <senyoupl@icloud.com>
2025-08-26 13:46:16 +0200

    fix(version): replace expired token by inepuisable one

--- apps/jobs/src/routes/version/lib/github/const.ts
+++ apps/jobs/src/routes/version/lib/github/const.ts
@@ -5,4 +5,4 @@ export const DELIVERY_IN_PROGRESS_ACTIONS_RUNS_URL = "https://api.github.com/rep
     This fine-grained GitHub access token does not expire
     and is not sensitive because it has no restricted access (read-only access to public repositories).
 */
-export const GH_ACCESS_TOKEN = "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";
+export const GH_ACCESS_TOKEN = "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";
